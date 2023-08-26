71734b1ea5a303fe47b522135ca84106534322d9
xupuzr <lopikop008@gmail.com>
2023-08-26 12:43:31 +0500

    xupuzr.com

---
+++ java.js
@@ -0,0 +1,12 @@
+if(age < 18){
+    alert('Вы еще молоды,вам нужно учится')
+}else if(age < 50){
+    alert('Вам нужно пахать')
+}else if(age < 59){
+    alert('Вам скоро на пенсию')
+}else if(age < 100){
+    alert('Вы пенсионер')
+}else{
+    (age = isNaN + age > 110 )
+    alert('Something going wrong.....')
+}
